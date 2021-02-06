49bcd8ae7ff49ca30480a425becacf1d3736d149
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-02-06 13:55:18 +0100

    placeholder for neural net-critic

---
+++ Project1/ActorCriticModel/NetCritic.py
@@ -0,0 +1,8 @@
+import tensorflow as tf 
+from tensorflow import keras 
+
+class NetCritic:
+    def __init__(self, shit = "nice"):
+        #do stuff
+        if shit == "nice":
+            self.stuff = "done"
