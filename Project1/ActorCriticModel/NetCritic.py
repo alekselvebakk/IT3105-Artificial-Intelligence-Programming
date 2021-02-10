d459d7fb72e56e11831e8c5f4db6fc41ef1a70d8
alekselvebakk <aleksnel@stud.ntnu.no>
2021-02-10 12:52:45 +0100

    fjerna placeholderkode og litt annet

--- Project1/ActorCriticModel/NetCritic.py
+++ Project1/ActorCriticModel/NetCritic.py
@@ -2,7 +2,12 @@ import tensorflow as tf
 from tensorflow import keras 
 
 class NetCritic:
-    def __init__(self, shit = "nice"):
-        #do stuff
-        if shit == "nice":
-            self.stuff = "done"
+    def __init__(self, alpha):
+        
+        
+
+        #Constants
+        self.alpha = tf.constant(alpha)
+
+
+    
