ca98bbf8b14cdf54493e61ba331411b4dd0bdbd9
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-02-11 16:43:44 +0100

    Begynt på implementasjon av netcritic

--- Project1/ActorCriticModel/NetCritic.py
+++ Project1/ActorCriticModel/NetCritic.py
@@ -1,13 +1,31 @@
 import tensorflow as tf 
-from tensorflow import keras 
+from tensorflow import keras
+from splitgd import SplitGD
 
 class NetCritic:
-    def __init__(self, alpha):
+    def __init__(self, alpha, layers):
         
         
 
+
+
+        ##### MODEL aka NEURAL NET CREATION #####
+        
+        #Initialize model
+        #Sequential model is just normal deep neural net model
+        self.model = keras.models.Sequential(name = 'SequentialCriticModel')
+
+        #Add hidden dense layers
+        #dense layers are just "every neuron connected to every other neuron"-layers
+        for i in len(layers)-1:
+            name = "layer"+i
+            current_layer = keras.layers.Dense(layers[i],activation='relu', name = name)
+            self.model.add(current_layer)
+            self.model.summary()
+        
+        #Add output layer, this will be used to classify
+        output_layer = keras.layers.Dense(layers[-1], activation = 'relu', name = "outputlayer")
+        self.model.add(output_layer)
+        
         #Constants
-        self.alpha = tf.constant(alpha)
-
-
-    
+        
