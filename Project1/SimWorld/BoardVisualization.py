cd535674dcd7adc081100caf66856051dde0ba33
Solveig <solvehl@stud.ntnu.no>
2021-02-11 15:33:31 +0100

    Visuaization class logic finished

--- Project1/SimWorld/BoardVisualization.py
+++ Project1/SimWorld/BoardVisualization.py
@@ -9,28 +9,51 @@ class BoardVisualization:
     def __init__(self, board):
         self.board = board
         self.G = nx.Graph()
+        self.node_pos = []
+        self.node_color = []
+        self.board_type = 'triangle' if board.__class__.__name__ == 'TriangleBoard' else 'diamond'
 
     def create_board_graph(self):
-        for row in self.board.table:
-            for peghole in row:
-                self.G.add_node(peghole)
-                self.add_neighbours(peghole)
+        # Information for the top node
+        row_pos = self.board.size
+        col_pos = self.board.size
+        top_node = self.board.table[0][0]
 
-    def add_neighbours(self, peghole):
+        self.add_node_with_properties(top_node, (col_pos, row_pos))  # Adds the first/top noe
+        self.add_down_neighbour_nodes(top_node, col_pos, row_pos)  # Function to recursively adds the other nodes
+
+
+
+    def add_neighbour_edges(self, peghole):
         for neighbour in peghole.neighbours.values():
             if neighbour in self.G.nodes:
                 self.G.add_edge(peghole, neighbour)
 
+    # adds the node with right position, color and edges to each of its existing neighbours
+    def add_node_with_properties(self, peghole, pos):
+        self.G.add_node(peghole, pos=pos)
+        self.node_color.append('blue') if peghole.filled else self.node_color.append('black')
+        self.add_neighbour_edges(peghole)
+
+    def add_down_neighbour_nodes(self, peghole, row_pos, col_pos):
+        down_neighbours = ["right", "down"] if self.board_type == 'diamond' else ["down", "rightdown"]
+        row = row_pos - 1
+        print("PEGHOLE", peghole)
+        for neighbour in peghole.neighbours:
+            neighbour_peghole = peghole.neighbours[neighbour]
+            print(neighbour)
+            if neighbour in down_neighbours and neighbour_peghole not in self.G.nodes:
+                col = col_pos-1 if neighbour == "down" else col_pos+1
+                self.add_node_with_properties(neighbour_peghole, (col, row))
+                self.add_down_neighbour_nodes(neighbour_peghole, row, col)
 
 def main():
-    db = DiamondBoard(6, [[2, 2]])
-    tb = TriangleBoard(4, [[2, 2]])
-    bv = BoardVisualization(tb)
+    db = DiamondBoard(5, [[0, 0], [0, 2]])
+    tb = TriangleBoard(8, [[0, 0], [1,1]])
+    bv = BoardVisualization(db)
     bv.create_board_graph()
-    print(bv.G.number_of_nodes())
-    print(bv.G.number_of_edges())
-    print(bv.G.nodes)
-    nx.draw(bv.G)
+    pos = nx.get_node_attributes(bv.G, 'pos')
+    nx.draw(bv.G, node_color=bv.node_color, pos=pos)
     plt.show()
 
 
