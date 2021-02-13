bd12d78033c0b6658176b7d9dcf0ca810193e400
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-02-13 11:39:45 +0100

    Skrevet utestet variant av netcritic

--- Project1/ActorCriticModel/NetCritic.py
+++ Project1/ActorCriticModel/NetCritic.py
@@ -1,16 +1,16 @@
 import tensorflow as tf 
 from tensorflow import keras
-from Project1.ActorCriticModel.splitgd import SplitGD
+import numpy as np
 
 class NetCritic:
-    def __init__(self, alpha, layers):
+    def __init__(self, alpha, gamma, elig_decay,layers):
         
-        
-
-
+        #Constants
+        self.alpha = tf.constant(alpha)
+        self.gamma = tf.constant(gamma)
+        self.elig_decay = tf.constant(elig_decay)
 
         ##### MODEL aka NEURAL NET CREATION #####
-        
         #Initialize model
         #Sequential model is just normal deep neural net model
         self.model = keras.models.Sequential(name = 'SequentialCriticModel')
@@ -18,14 +18,43 @@ class NetCritic:
         #Add hidden dense layers
         #dense layers are just "every neuron connected to every other neuron"-layers
         for i in len(layers):
-            name = "layer"+i
-            current_layer = keras.layers.Dense(layers[i],activation='relu', name = name)
+            layer_name = "hidden_layer"+i
+            current_layer = keras.layers.Dense( layers[i],
+                                                activation='relu', 
+                                                name = layer_name)
             self.model.add(current_layer)
             self.model.summary()
-        
-        #Add output layer, this will be used to classify
-        output_layer = keras.layers.Dense(1, activation = 'sigmoid', name = "outputlayer")
+
+        #Add output layer, this will be used to get the value
+        output_layer = keras.layers.Dense(  1, 
+                                            activation = 'sigmoid', 
+                                            name = "outputlayer")
         self.model.add(output_layer)
-        
-        #Constants
+        optim = keras.optimizers.SGD(learning_rate=self.alpha)
+        self.model.compile(optim)
+
+        #Set initial eligibilities
+        self.elig = tf.Variable()
+
+    def episode_reset(self):
+        weights = self.model.trainable_weights
+        self.elig = tf.zeros_like(weights)
+
+    def get_delta(self, state, next_state, reward):
+        V_s = self.model(state)
+        V_s_next = self.model(next_state)
+        delta = reward + self.gamma*V_s_next-V_s
+        return delta
+
+    def update_value_function(self, state, delta):
+        #This is the custom fit-function
+        params = self.model.trainable_weights
+        with tf.GradientTape() as tape:
+            V_s = self.model(state)
+            dV_dw = tape.gradient(V_s, params)
+            self.elig = self.elig + dV_dw
+            update = delta*self.elig
+            self.model.optimizer.apply_gradients(   update, 
+                                                    self.model.trainable_weights)
+        self.elig = self.elig*self.elig_decay
         
