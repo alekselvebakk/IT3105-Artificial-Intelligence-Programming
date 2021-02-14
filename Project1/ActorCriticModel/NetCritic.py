52fd23127c3697acbe995a273ff3be1a169afd35
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-02-14 21:28:26 +0100

    LIVET E VANSKELIG OG TUNGT

--- Project1/ActorCriticModel/NetCritic.py
+++ Project1/ActorCriticModel/NetCritic.py
@@ -10,9 +10,11 @@ class NetCritic:
     def __init__(self, alpha, gamma, elig_decay, layers, input_size):
         
         #Constants
-        self.alpha = tf.constant(alpha)
-        self.gamma = tf.constant(gamma)
-        self.elig_decay = tf.constant(elig_decay)
+        self.alpha = alpha
+        self.gamma = gamma
+        self.elig_decay = elig_decay
+        self.layers = layers
+
 
         ##### MODEL aka NEURAL NET CREATION #####
         #Initialize model
@@ -20,51 +22,53 @@ class NetCritic:
         self.model = keras.models.Sequential(name = 'SequentialCriticModel')
 
         #Input layers
-        input_layer = keras.layers.Dense(   layers[0], 
+        print(layers[0])
+        input_layer = keras.layers.Dense(   self.layers[0], 
                                             input_shape=(input_size,),
                                             name = 'hidden_layer0')
         self.model.add(input_layer)
-        print("-----",self.model.input_shape)
         #Hidden layers
-        for i in range(len(layers)-1):
+        for i in range(len(self.layers)-1):
             layer_name = "hidden_layer"+str(i+1)
-            current_hidden = keras.layers.Dense(layers[i+1],
+            current_hidden = keras.layers.Dense(self.layers[i+1],
                                                 activation='relu', 
                                                 name = layer_name)
-            #self.model.add(current_hidden)
+            self.model.add(current_hidden)
         
         #Outputlayer
         output_layer = keras.layers.Dense(  1, 
                                             activation = 'sigmoid', 
                                             name = "outputlayer")
         #self.model.add(output_layer)
-        #self.model.summary()
+        self.model.summary()
 
-        #optim = keras.optimizers.SGD(learning_rate=self.alpha)
-        #self.model.compile(optimizer = optim,)
-        #config = self.model.get_config()
-        #print(config["layers"][0]["config"]["batch_input_shape"])
+        optim = keras.optimizers.SGD(learning_rate=self.alpha)
+        self.model.compile(optimizer = optim)
+        config = self.model.get_config()
+        print(config["layers"][0]["config"]["batch_input_shape"])
         #Set initial eligibilities
         weights = self.model.trainable_weights
-        self.elig = []
+        self.elig =[]
         for i in range(len(weights)):
             self.elig.append(tf.zeros_like(weights[i]))
+        print(weights)
+        print(self.elig)
 
     @staticmethod
     def string_to_tensor(string_variable):
-        variable = np.array([list(string_variable)], dtype=int)
+        content = list(string_variable)
+        print(content)
+        variable = np.array([content], dtype=int)
         #variable = variable.reshape(len(variable),1)
         #variable = tf.Variable(tf.constant(variable), shape = tf.TensorShape([len(variable),1]))
         return variable
 
     def episode_reset(self):
-        for i in range(len(self.model.trainable_weights)):
-            self.elig.append(tf.zeros_like(self.model.trainable_weights[i]))
-
+        a = 1#tf.math.scalar_mul(0, self.elig)
 
     def get_delta(self, state, next_state, reward):
         state = self.string_to_tensor(state)
-        print("****", state.shape)
+        print("*******delta func: ",state)
         V_s = self.model(state)
         next_state = self.string_to_tensor(next_state)
         V_s_next = self.model(next_state)
@@ -77,9 +81,12 @@ class NetCritic:
         params = self.model.trainable_weights
         with tf.GradientTape() as tape:
             V_s = self.model(state)
+            print("----------", V_s)
+            print("*********", params)
             dV_dw = tape.gradient(V_s, params)
+            print("................", dV_dw)
             self.elig = self.elig + dV_dw
-            update = delta*self.elig
+            update = tf.math.scalar_mul(delta,self.elig)
             self.model.optimizer.apply_gradients(   update, 
                                                     self.model.trainable_weights)
         self.elig = self.elig*self.elig_decay
