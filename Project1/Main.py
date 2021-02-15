6236e48581224db41cebe65b431422e24b7d8e2a
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-02-15 15:50:44 +0100

    lagd en ny "basic episode"

--- Project1/Main.py
+++ Project1/Main.py
@@ -48,20 +48,21 @@ def main():
     decays = 0
 
     for i in range(episodes):
-        run_episode(learning_module, game_handler)
+        if float(i)/episodes*100%1==0:
+            pros = float(i)/episodes*100
+            print(pros, "% ferdig")
+        run_learning_episode(learning_module, game_handler)
         performance[i] = game_handler.board.num_pegs
         if i > 0 and performance[i] < performance[i - 1]:
             decays = decays + 1
             learning_module.decay_epsilon()
-
-    learning_module.decay_epsilon(zero=True)
-    run_episode(learning_module, last_game_handler)
-
+    print(100, "% ferdig")
     plt.plot(performance)
     plt.show()
+  
 
 
-def run_episode(learning_module, game_handler):
+def run_learning_episode(learning_module, game_handler):
     game_handler.reset_board()
     action = learning_module.initialize_episode(game_handler.get_board_state(), game_handler.get_actions())
     while not game_handler.check_if_final_state()[0]:
@@ -71,8 +72,17 @@ def run_episode(learning_module, game_handler):
                                               game_handler.get_actions(),
                                               reward,
                                               next_state_is_final=game_handler.check_if_final_state()[0])
+    game_handler.visualize_board()
 
 
+def run_basic_episode(learning_module, game_handler):
+    game_handler.reset_board()
+    action = learning_module.initialize_episode(game_handler.get_board_state(), game_handler.get_actions())
+    game_handler.perform_action(action)
+    while not game_handler.check_if_final_state()[0]:
+        action = learning_module.basic_step(game_handler.get_board_state(), game_handler.get_actions())
+        game_handler.perform_action(action)
+    game_handler.visualize_board()
 
 if __name__ == '__main__':
     main()
