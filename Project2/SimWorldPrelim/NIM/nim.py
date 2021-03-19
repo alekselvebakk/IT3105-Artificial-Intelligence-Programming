e2c6b84d176f891ab9f7bb7e5c7bca81a1d6deb0
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-03-19 12:13:25 +0100

    startet implementasjon av prelim-games

---
+++ Project2/SimWorldPrelim/NIM/nim.py
@@ -0,0 +1,7 @@
+
+
+class nim():
+    def __init__(N, K):
+        self.N = N #Number of pieces on the board initially 
+        self.K = K #Maximum number of pieces a player can remove from the board
+        
