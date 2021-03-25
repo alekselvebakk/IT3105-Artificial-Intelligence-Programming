3e9db2256d89ae73b76941fa134e4ea3aa39496f
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-03-25 13:44:17 +0100

    lagt til attributes i MCTS-noden

--- Project2/MCTS/MCTS_Node.py
+++ Project2/MCTS/MCTS_Node.py
@@ -2,8 +2,12 @@
 
 
 class MCTS_Node():
-    def __init__(state):
+    def __init__(state, root = False):
         self.N_s = 0
         self.state = state
+        self.root = root
+        self.Q_s_a = dict()
+        self.N_s_a = dict()
+        self.leaf = True
 
         
