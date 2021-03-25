fda0fb0a511e1b92d7b51b48c9ab486af5c5cd2f
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-03-25 13:43:32 +0100

    ryddet litt i tre-klassen

--- Project2/MCTS/MCTS_Tree.py
+++ Project2/MCTS/MCTS_Tree.py
@@ -4,9 +4,4 @@ import MCTS_Node
 
 class MCTS_Tree():
     def __init__(state):
-        self.root = MCTS_Node(state)
-    
-    def traverse_tree():
-        
-
-
+        self.root = MCTS_Node(state, root = True)
