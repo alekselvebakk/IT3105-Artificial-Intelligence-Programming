2854ac71d15c17f6574cd1616f25dbfa4e695b2a
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-03-27 13:41:10 +0100

    uferdige endringer, bare tull

--- Project2/MCTS/MCTS_Node.py
+++ Project2/MCTS/MCTS_Node.py
@@ -2,12 +2,19 @@
 
 
 class MCTS_Node():
-    def __init__(state, root = False):
+    def __init__(state = state, parent = None):
         self.N_s = 0
         self.state = state
-        self.root = root
-        self.Q_s_a = dict()
-        self.N_s_a = dict()
         self.leaf = True
+    
+    def increment_N_s():
+        self.N_s()
+    
+    def prune_parent():
+        self.parent = None
 
-        
+    def get_best_action(action_list):
+       if self.leaf:
+
+    def is_leaf():
+        return self.leaf
