9cabf9023571c3be9bc754a71c66e89b0da45b0c
Aleksander Elvebakk <aleksnel@stud.ntnu.no>
2021-04-02 19:41:39 +0200

    startet  på å programmere tresøk-logikk

--- Project2/MCTS/MCTS.py
+++ Project2/MCTS/MCTS.py
@@ -1,10 +1,24 @@
 class MCTS():
-    def __init__(GameHandler, number_search_games):
+    def __init__(GameHandler, number_search_games, root):
         self.GameHandler = GameHandler
-        state = GameHandler.get_state()
-        self.tree = MCTS_Tree(state)
+        self.root = root
+        self.GameHandler.set_state(self.root)
+        actions = self.GameHandler.get_actions
+        root_node = MCTS_Node(state = state, actions = actions)
+        self.tree = dict()
+        self.tree[self.root] = root_node
         self.number_search_games = number_search_games
 
-    def perform_search_game():
-        possible_actions = GameHandler.get_actions()
-        action = self.tree.get_action()
+
+    def tree_simulation(c = 1):
+        simulation_order = []
+        self.GameHandler.set_state(self.root)
+        action = self.tree[self.root].get_action(c)
+        self.GameHandler.perform_action(action)
+        s_t = GameHandler.get_action
+        if 
+
+        while not self.GameHandler.state_is_final()
+            simulation_order.append()
+            if 
+        
