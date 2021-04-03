19b44845fb75b4589948938b1e41e209e8425f0b
Solveig <solvehl@stud.ntnu.no>
2021-04-03 12:44:43 +0200

    Started hex game, board finished

--- Project2/Actor.py
+++ Project2/Actor.py
@@ -20,7 +20,7 @@ class Actor:
         self.opt = eval('keras.optimizers.' + opt)
         self.act = act
         self.last_act = last_act
-        self.input_size = 1 + board_size #The state will be playerID + board
+        self.input_size = 1 + board_size # The state will be playerID + board
         self.output_size = board_size
 
         # makes a new NN or reloads from earlier trained model
@@ -31,7 +31,8 @@ class Actor:
         input = keras.layers.Input(shape=(self.input_size,), name='input_layer')
         x = input
         for i in range(len(self.layers)):
-            x = keras.layers.Dense(self.layers[i], activation=self.act, kernel_initializer='zeros', bias_initializer='zeros')(x)
+            x = keras.layers.Dense(self.layers[i], activation=self.act, name='hidden_'+str(i),
+                                   kernel_initializer='zeros', bias_initializer='zeros')(x)
         output = keras.layers.Dense(self.output_size, activation=self.last_act, name='output_layer',
                                     kernel_initializer='zeros', bias_initializer='zeros')(x)
         model = keras.models.Model(input, output)
@@ -40,11 +41,10 @@ class Actor:
         print(model.trainable_weights)
         return model
 
-    # TODO: save and reload
     def save_net(self, net_name):
         self.model.save('Saved_models/' + net_name)
 
-    # loss function from Keith, but TODO: what does it mean?
+    # loss function from Keith, but TODO: what does it mean? and does it work?
     def cross_entropy(self, targets, outputs):
         return tf.reduce_mean(tf.reduce_sum(-1*targets*self.safelog(outputs), axis=1))
 
@@ -73,7 +73,7 @@ class Actor:
 
         # Scaling of the remaining numbers to the sum of 1
         prediction = prediction/prediction.sum()
-       # print('PRED_NORM:', prediction)
+        # print('PRED_NORM:', prediction)
 
         return prediction
 
@@ -102,7 +102,6 @@ class Actor:
 
 def main():
     a = Actor()
-    #a.model('12345678901234567890123456', training=False)
     a.get_probabilities('21201201201201201201201201')
 
 
---
+++ Project2/SimWorld/Peghole.py
@@ -0,0 +1,29 @@
+class Peghole:
+    def __init__(self, row, column, filled):
+        self.row = row
+        self.column = column
+        self.filled = filled
+        self.neighbours = {}
+
+    def add_neighbour(self, direction, peghole):
+        self.neighbours[direction] = peghole
+
+    def find_neighbour_direction(self, neighbour_row, neighbour_column):
+        direction = ""
+        if neighbour_column - self.column <= -1:
+            direction += "left"
+        elif neighbour_column - self.column >= 1:
+            direction += "right"
+
+        if neighbour_row - self.row <= -1:
+            direction += "up"
+        elif neighbour_row - self.row >= 1:
+            direction += "down"
+
+        return direction
+
+    def remove_peg(self):
+        self.filled = None
+
+    def add_peg(self, player):
+        self.filled = player
